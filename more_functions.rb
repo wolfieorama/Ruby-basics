57f15bcbc9ec4c729ca171c5a023fcf8a6f29980
wolfieorama <wachirawamunyi@gmail.com>
2015-08-10 01:15:09 +0300

    more basics on functions

---
+++ more_functions.rb
@@ -0,0 +1,17 @@
+def factors_to_three(n)
+
+	remainder = n % 3
+
+	if remainder == 0
+		return true
+	else
+		return false
+	end
+end
+
+factors_to_three(21)
+factors_to_three(99)
+factors_to_three(878667)
+factors_to_three(754555)
+factors_to_three(33)
+factors_to_three(40)
