a82b091a0ead242ce8d70b4f6e38b7831f2d7c42
wolfieorama <wachirawamunyi@gmail.com>
2015-08-17 16:36:23 +0300

    using require relative to add momdules

---
+++ longvowel.rb
@@ -0,0 +1,12 @@
+
+require_relative '/home/wolfieorama/Desktop/ruby_modules/longest_vowel'
+
+
+def longvowel(sen)
+
+  longest = Longest_vowel.longest_word(sen)
+  vowels = Longest_vowel.count_vowels(longest)
+
+end
+
+puts longvowel("this is why we all love sukumawiki")
