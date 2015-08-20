47fd6e955c217415c1df553aca434508f6aa53a5
wolfieorama <wachirawamunyi@gmail.com>
2015-08-20 14:05:02 +0300

    added custom message & dep, .gs gems

--- frogger/app.rb
+++ frogger/app.rb
@@ -2,6 +2,12 @@ require "cuba"
 
 Cuba.define do
   on root do
-    res.write("Hello Frogger!")
+    res.write("
+    <html>
+        <body>
+          <h1>Welcome to this super Awesome App</h1>
+        </body>
+    </html>
+    ")
   end
 end
