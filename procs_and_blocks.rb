d8548d08c1812b3c133b551b825d7f39ba2994bd
wolfieorama <wachirawamunyi@gmail.com>
2015-08-22 11:44:55 +0300

    procs taking parameters

--- procs_and_blocks.rb
+++ procs_and_blocks.rb
@@ -1,17 +1,17 @@
-toast = Proc.new do
-  puts "procs have never been easy"
-  puts "procs take a ,block of code and wrap it"
-  puts "procs are like functions in that you can call them"
-  puts "procs dont have objects bound to them"
-  puts "procs are objects"
-  puts "procs is a short for procedure"
+toast = Proc.new do |numbers|
+  puts "#{number} procs have never been easy"
+  puts "#{number} procs take a ,block of code and wrap it"
+  puts "#{number} procs are like functions in that you can call them"
+  puts "#{number} procs dont have objects bound to them"
+  puts "#{number} procs are objects"
+  puts "#{number} procs is a short for procedure"
 end
 
-toast.call
+toast.call 'one'
 puts
 puts "==========================================="
-toast.call
+toast.call 'two'
 puts "==========================================="
-toast.call
+toast.call 'three'
 puts "==========================================="
-toast.call
+toast.call 'four'
