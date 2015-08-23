c4ecf6ae8f312504fcd381b99e3110462db0b9a0
wolfieorama <wachirawamunyi@gmail.com>
2015-08-23 21:15:24 +0300

    added views in cuba

--- frogger/app.rb
+++ frogger/app.rb
@@ -1,7 +1,11 @@
 require "cuba"
+require "mote"
+require "mote/render"
+
+Cuba.plugin(Mote::Render)
 
 Cuba.define do
   on root do
-    res.write("Hello Frogger!")
+    res.write ("hello frogger!!!")
   end
 end
